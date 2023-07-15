99be45550e38d63c49a633ed39bd269905ea8011
judeclark19 <judeclark2019@gmail.com>
2023-07-15 15:00:38 -0400

    feat: added custom favicon
    In order to make the favicon work with no errors, I had to add a line to the head via layout.tsx and do some combination of hard refresh/delete .next folder/restart server in order for it to render with no errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className={inter.className}>
         <main id="main-parent">
           <Header />
